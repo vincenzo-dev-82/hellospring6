3418055b0c4497fdb563625882741a782623384a
vincenzo <vincenzo.911gts@gmail.com>
2024-08-01 23:17:32 +0900

    URL 클래스로 환율 가져오기

--- src/main/java/com/vincenzo/hellospring/PaymentService.java
+++ src/main/java/com/vincenzo/hellospring/PaymentService.java
@@ -1,19 +1,35 @@
 package com.vincenzo.hellospring;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
 
         // 환율 가져오기
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = bufferedReader.lines().collect(Collectors.joining());
+        bufferedReader.close();
+
+        System.out.println(response);
+
         // 금액 계산
         // 유효 시간 계산
 
         return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
