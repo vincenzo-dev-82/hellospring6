c63f761e4a9f3d41456a260819a1777d95b00b1a
vincenzo <vincenzo.911gts@gmail.com>
2024-08-03 00:59:07 +0900

    원칙과 패턴 : sort를 활용하여 Strategy Pattern 설명

---
+++ src/main/java/com/vincenzo/hellospring/Sort.java
@@ -0,0 +1,32 @@
+package com.vincenzo.hellospring;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Sort {
+
+    public static void main(String[] args) {
+
+        List<String> scores1 = Arrays.asList("z", "a", "spring", "java");
+        Collections.sort(scores1);
+        scores1.forEach(System.out::println);
+
+        // Comparator를 사용한 비교
+        List<String> scores2 = Arrays.asList("z", "a", "spring", "java");
+        Collections.sort(scores2, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        scores2.forEach(System.out::println);
+
+        // 람다식
+        List<String> scores3 = Arrays.asList("z", "a", "spring", "java");
+        Collections.sort(scores3, (o1, o2) -> o1.length() - o2.length());
+        scores3.forEach(System.out::println);
+    }
+
+}
