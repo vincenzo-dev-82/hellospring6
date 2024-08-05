5b99193b0bd2b8e293d9b3bf43e77ad740eeda08
vincenzo <vincenzo.911gts@gmail.com>
2024-08-05 21:56:13 +0900

    테스트 : 테스트와DI(2) - ContextConfiguration과 Autowired를 사용하여 BeanFactory 사용

--- src/test/java/com/vincenzo/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/com/vincenzo/hellospring/payment/PaymentServiceSpringTest.java
@@ -1,23 +1,29 @@
 package com.vincenzo.hellospring.payment;
 
-import com.vincenzo.hellospring.ObjectFactory;
 import com.vincenzo.hellospring.TestObjectFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
+
+    @Autowired
+    BeanFactory beanFactory;
 
     @Test
     @DisplayName("convertedAmount 메서드가 요구사항 3가지를 잘 충족했는지 검증")
     void convertedAmount() throws IOException {
-
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
+//        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         // 환율정보를 가져온다
