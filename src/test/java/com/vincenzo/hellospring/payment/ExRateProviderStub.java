eb29c441ffee9332f81edd90ca8239d1a2cf14ca
vincenzo <vincenzo@sk.com>
2024-08-05 14:14:54 +0900

    테스트 : 테스트와DI(1)

---
+++ src/test/java/com/vincenzo/hellospring/payment/ExRateProviderStub.java
@@ -0,0 +1,28 @@
+package com.vincenzo.hellospring.payment;
+
+import com.vincenzo.hellospring.exrate.ExRateProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class ExRateProviderStub implements ExRateProvider {
+
+    private BigDecimal exRate;
+
+    public ExRateProviderStub(BigDecimal exRate) {
+        this.exRate = exRate;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public void setExRate(BigDecimal exRate) {
+        this.exRate = exRate;
+    }
+
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException {
+        return exRate;
+    }
+}
