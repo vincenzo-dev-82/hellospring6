9723edd7d75491d6bf09293d830706be6822d37c
vincenzo <vincenzo.911gts@gmail.com>
2024-08-05 22:29:32 +0900

    학습테스트 : Clock을 사용해서 유동적 시간과 고정시간을 활용한 테스트

---
+++ src/test/java/com/vincenzo/hellospring/learningtest/ClockTest.java
@@ -0,0 +1,39 @@
+package com.vincenzo.hellospring.learningtest;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class ClockTest {
+
+    @DisplayName("Clock을 이용해서 LocalDateTime.now?")
+    @Test
+    void clock() {
+        Clock clock = Clock.systemDefaultZone();
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+
+        Assertions.assertThat(dt2).isAfter(dt1);
+    }
+
+
+    @DisplayName("Clock을 테스트에서 사용할 때 내가 원하는 시간을 지정해서 현재 시간을 가져오게 할 수 있는가?")
+    @Test
+    void fixedClock() {
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        LocalDateTime dt1 = LocalDateTime.now(clock);
+        LocalDateTime dt2 = LocalDateTime.now(clock);
+        LocalDateTime dt3 = LocalDateTime.now(clock).plusHours(1);
+
+        Assertions.assertThat(dt2).isEqualTo(dt1);
+        Assertions.assertThat(dt3).isEqualTo(dt1.plusHours(1));
+    }
+}
