1bd0e353bfc3ad54530611035c64dc9b78733ff2
vincenzo <vincenzo.911gts@gmail.com>
2024-08-05 22:02:47 +0900

    테스트 : 테스트와DI(2) - PaymentService를 Autowired하여 테스트 실행

--- src/test/java/com/vincenzo/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/com/vincenzo/hellospring/payment/PaymentServiceSpringTest.java
@@ -5,7 +5,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -17,14 +16,11 @@ import java.math.BigDecimal;
 @ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
 
-    @Autowired
-    BeanFactory beanFactory;
+    @Autowired PaymentService paymentService;
 
     @Test
     @DisplayName("convertedAmount 메서드가 요구사항 3가지를 잘 충족했는지 검증")
     void convertedAmount() throws IOException {
-//        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
-        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         // 환율정보를 가져온다
         Assertions.assertThat(payment.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(1_000));
