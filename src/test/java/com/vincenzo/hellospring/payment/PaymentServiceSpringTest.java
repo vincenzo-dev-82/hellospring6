5ec663b734e47ec31c16731085a87ddb27c9881d
vincenzo <vincenzo.911gts@gmail.com>
2024-08-05 22:09:16 +0900

    테스트 : 테스트와DI(2) - ExRateProviderStub을 활용한 케이스 추가

--- src/test/java/com/vincenzo/hellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/com/vincenzo/hellospring/payment/PaymentServiceSpringTest.java
@@ -17,15 +17,25 @@ import java.math.BigDecimal;
 class PaymentServiceSpringTest {
 
     @Autowired PaymentService paymentService;
+    @Autowired ExRateProviderStub exRateProviderStub;
 
     @Test
     @DisplayName("convertedAmount 메서드가 요구사항 3가지를 잘 충족했는지 검증")
     void convertedAmount() throws IOException {
+        // exRate : 1000
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         // 환율정보를 가져온다
         Assertions.assertThat(payment.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(1_000));
         // 원화환산금액 계산
         Assertions.assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
+
+
+        // exRate : 500
+        exRateProviderStub.setExRate(BigDecimal.valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+        Assertions.assertThat(payment2.getExRate()).isEqualByComparingTo(BigDecimal.valueOf(500));
+        Assertions.assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(5_000));
+
 
         // 원화환산금액의 유효시간 계산
 //        Assertions.assertThat(payment.getValidUnit()).isAfter(LocalDateTime.now());
