b9ca7dc00b9a297e0dfc4a0f50cee744b34ea0cd
vincenzo.dev.82 <vincenzo.dev.82@gmail.com>
2024-08-07 10:30:15 +0900

    섹션6:서비스 추상화:트랜잭션 서비스 추상화

--- src/main/java/com/vincenzo/hellospring/order/OrderService.java
+++ src/main/java/com/vincenzo/hellospring/order/OrderService.java
@@ -2,6 +2,7 @@ package com.vincenzo.hellospring.order;
 
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
@@ -10,9 +11,9 @@ import java.math.BigDecimal;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final JpaTransactionManager transactionManager;
+    private final PlatformTransactionManager transactionManager;
 
-    public OrderService(OrderRepository orderRepository, JpaTransactionManager transactionManager) {
+    public OrderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
         this.orderRepository = orderRepository;
         this.transactionManager = transactionManager;
     }
